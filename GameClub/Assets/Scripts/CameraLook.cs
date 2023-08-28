fa19886ea6775455dc6c4ea1fb44cc4d43d6938e
Pranav Sukesh <pranavsukesh@gmail.com>
2023-08-27 18:40:47 -0700

    Fixed FPS Movement
    Modified player movement script to have less jank physics
    everything parameterized in unity editor
    need to fix throw mechanic so the object actually shoots straight

--- GameClub/Assets/Scripts/CameraLook.cs
+++ GameClub/Assets/Scripts/CameraLook.cs
@@ -5,24 +5,17 @@ using UnityEngine;
 public class CameraLook : MonoBehaviour {
     public float mouseSensitivity = 300f;
     public Transform playerBody;
-    float targetXRotation = 0f;
+
     float currentXRotation = 0f;
     public float rotationSmoothness = 5f;
     public float PitchAngle { get { return -currentXRotation; } }
 
     void Start() {
-        Cursor.lockState = CursorLockMode.Locked;
-        targetXRotation = transform.localRotation.eulerAngles.x;
-        currentXRotation = targetXRotation;
+        currentXRotation = transform.localRotation.eulerAngles.x;
     }
 
     void Update() {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        targetXRotation -= mouseY;
-        targetXRotation = Mathf.Clamp(targetXRotation, -90f, 90f);
-        currentXRotation = Mathf.Lerp(currentXRotation, targetXRotation, rotationSmoothness);
-        transform.localRotation = Quaternion.Euler(currentXRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        
+        currentXRotation = transform.localRotation.eulerAngles.x;
     }
 }
--- GameClub/Assets/Scripts/PlayerMovement.cs
+++ GameClub/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+
+    /*
     public float WALK_SPEED = 12f;
     public float SPRINT_SPEED = 17f;
     private float speed;
@@ -14,31 +16,126 @@ public class PlayerMovement : MonoBehaviour {
     public LayerMask groundMask;
     Vector3 velocity;
     bool isGround;
+    */
+
+    public bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+    public bool ShouldJump => Input.GetKey(jumpKey) && characterController.isGrounded;
+
+
     public CameraLook cameraLook;
 
+    [Header("Functional Options")]
+    [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canJump = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Movement Parameters")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintSpeed = 6.0f;
+
+
+    [Header("Look Parameters")]
+    [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
+    [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
+    [SerializeField, Range(1, 100)] private float upperLookLimit = 80.0f;
+    [SerializeField, Range(1, 100)] private float lowerLookLimit = 80.0f;
+
+    [Header("Jumping Parameters")]
+    [SerializeField] private float gravity = 30.0f;
+    [SerializeField] private float jumpForce = 8.0f;
+
+
+    private Camera playerCamera;
+    private CharacterController characterController;
+
+    private Vector3 moveDirection;
+    private Vector2 currentInput;
+
+    private float rotationX = 0;
+
+    private Vector3 spawnPoint;
+
+    public enum PlayerState { CanMove };
+    public PlayerState playerState;
+
     void Start() {
-        speed = WALK_SPEED;
         cameraLook = GetComponentInChildren<CameraLook>();
+
+        playerCamera = GetComponentInChildren<Camera>();
+        characterController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        playerState = PlayerState.CanMove;
     }
 
     void Update() {
-        isGround = Physics.CheckSphere(ground.position, groundDist, groundMask);
-        if (isGround && velocity.y < 0) {
-            velocity.y = -2f;
+        
+
+        switch (playerState)
+        {
+            case PlayerState.CanMove:
+                HandleMovementInput();
+                HandleMouseLook();
+                if (canJump)
+                {
+                    HandleJump();
+                }
+               
+                ApplyFinalMovements();
+                break;
         }
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+    }
 
-        Vector3 move = transform.right * x + transform.forward * z;
 
-        if (Input.GetKey(KeyCode.LeftShift)) {
-            speed = SPRINT_SPEED;
-        } else {
-            speed = WALK_SPEED;
+    /*
+    * Detects user input and sets the movement vector based on key inputs and speeds based on current actions.
+    */
+    private void HandleMovementInput()
+    {
+        currentInput = new Vector2((IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"),
+            (IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
+
+        float moveDirectionY = moveDirection.y;
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
+        moveDirection.y = moveDirectionY;
+    }
+
+    /*
+     * Detects mouse movement and rotates the camera accordingly to match vision. Also clamps movement to a certain view range.
+     */
+    private void HandleMouseLook()
+    {
+        rotationX -= Input.GetAxis("Mouse Y") * lookSpeedY;
+        rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
+    }
+
+    /*
+     * Applies jump force to the user if they are grounded and the right key was pressed.
+     */
+    private void HandleJump()
+    {
+        if (ShouldJump)
+            moveDirection.y = jumpForce;
+    }
+
+
+    private void ApplyFinalMovements()
+    {
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
         }
+        if (characterController.velocity.y < -0.01f && characterController.isGrounded)
+        {
+            moveDirection.y = 0;
 
-        controller.Move(move * speed * Time.deltaTime);
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        }
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
